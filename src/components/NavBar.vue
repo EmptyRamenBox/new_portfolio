f6db438dca8093dfb9a54eed07d9486d43802fb2
EmptyRamenBox <EmptyRamenBox.alexis.martinezo92@gmail.com>
2020-03-06 03:05:22 -0600

    Added NavBar and Home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav w-screen z-20 shadow-2xl bg-teal-600 text-blue-100">
+    <div class="left">
+        <span class="font text-4xl">My
+            <span class="font-semibold">Portfolio</span>
+        </span>
+    </div>
+    <div class="right">
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/">Portfolio</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+    @apply h-24 fixed px-12 flex justify-between items-center;
+
+}
+.link {
+    @apply px-2 text-lg font-semibold;
+    &:hover {
+        @apply bg-teal-800;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="home">
-    <h1>Hello There!</h1>
+  <div class="h-screen w-screen bg-cover bg-center shadow-2xl"
+    :style="`background-image:url(${bgImage})`"
+  >
+    <div class="h-screen w-screen bg-teal-900 opacity-75"/>
+
+    <div class="absolute h-screen w-screen top-0 flex flex-col justify-center items-center">
+      <h1 class="text-6xl font-bold text-teal-200 hover:bg-teal-600 hover:text-blue-200 px-4 hover:opacity-50">Alexis Martinez</h1>
+      <h3 class="text-3xl font-bold text-teal-200 hover:bg-teal-600 hover:text-blue-200 px-4 hover:opacity-50">Jr. Network Technician</h3>
+    </div>
   </div>
 </template>
 
 <script>
+import bgImage from "../assets/background.jpg";
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      bgImage
+    };
+  }
 };
 </script>
